501df8f7961a4b40950272dd79ba15c4e234b489
A01252618 <A01252618@itesm.mx>
2021-08-20 16:44:25 +0000

    quiz 1 distancia

--- assignments/14DistanciaDosPuntos/src/exercise.py
+++ assignments/14DistanciaDosPuntos/src/exercise.py
@@ -2,12 +2,12 @@ import math
 def main():
     #escribe tu código abajo de esta línea
     print("punto inicial:")
-    x1= int(input("x1= "))
-    y1= int(input("y1= "))
+    x1=int(input("x1= "))
+    y1=int(input("y1= "))
     
     print("punto final:")
-    x2= int(input("x2= "))
-    y2= int(input("y2= "))
+    x2=int(input("x2= "))
+    y2=int(input("y2= "))
     
     distancia= float(math.sqrt((x2-x1)**2 +(y2-y1)**2))
     distancia= round(distancia,2)
